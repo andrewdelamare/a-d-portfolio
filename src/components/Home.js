baa2c354d8d6c908d6d8f459c905b6b3d639aced
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-05-13 17:18:50 +0300

    fix shadow overflow issue

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,10 @@ import profilePic from "../images/profilePic.jpg"
 export const Home = () => {
 
   return (
-    <div className="flex mt-64 mb-32 justify-center mx-auto " id="#">
-      <div className="grid gap-16 grid-cols-2 grid-rows-4 text-bottom  px-20 pt-10 md:w-2/3 xs:w-full  ">
+    <div className="flex mt-64 mb-32 justify-center mx-auto overflow-visible" id="#">
+      <div className="grid grid-cols-2 grid-rows-4 text-bottom overflow-visible px-20 pt-10 md:w-2/3 xs:w-full  ">
         <div className=" text-5xl col-span-1 text-center self-center">Hi, I'm Andrew</div>
-        <img src={profilePic} alt="me" className="rounded-full drop-shadow-2xl border-4 border-zinc-700 w-40 justify-self-center"/>
+        <img src={profilePic} alt="me" className="rounded-full drop-shadow-2xl border-4 m-10 border-zinc-700 w-40 justify-self-center"/>
         <DownArrowIcon className="animate-bounce self-center w-full col-span-2" />
       </div>
     </div>  
