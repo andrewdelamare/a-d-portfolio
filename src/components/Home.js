264e1700ed2f35166a66eff9e7c1291b18c30905
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-05-18 16:28:37 +0300

    responsive home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,11 @@ import profilePic from "../images/profilePic.jpg"
 export const Home = () => {
 
   return (
-    <div className="flex mt-32 mb-32 justify-center mx-auto overflow-visible" id="home">
-      <div className="grid grid-rows-3 grid-cols-1 xs:grid-cols-2 text-bottom overflow-visible xs:px-20 pt-10 sm:w-2/3 w-full  ">
-        <div className=" text-4xl xs:text-5xl sm:col-span-1 text-center self-center">Hi, I'm Andrew</div>
-        <div className="xs:hidden"></div><img src={profilePic} alt="me" className="col-span-1 rounded-full border-4 m-10 border-zinc-700 w-40 justify-self-center"/>
+    <div className="flex mt-24 sm:mt-32 mb-40 sm:mb-32 justify-center mx-auto overflow-visible" id="home">
+      <div className="grid grid-cols-2 text-bottom overflow-visible xs:px-20 pt-10 sm:w-2/3 w-full  ">
+        <div className=" invisible sm:visible sm:text-5xl sm:col-span-1 text-center self-center">Hi, I'm Andrew</div>
+        <img src={profilePic} alt="me" className="col-span-2 sm:col-span-1 rounded-full border-4 m-10 border-zinc-700 w-60 sm:w-40 justify-self-center"/>
+        <div className="visible sm:invisible text-3xl col-span-2 text-center self-center mb-12">Hi, I'm Andrew</div>
         <DownArrowIcon className="animate-bounce self-center w-full h-7 col-span-2" />
       </div>
     </div>  
