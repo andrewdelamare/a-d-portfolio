477856bb52e4b2008c824f5810679132424a1769
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-10-14 19:15:38 +0300

    Fix child hover styling issue

--- src/components/ImgCard.js
+++ src/components/ImgCard.js
@@ -1,17 +1,17 @@
 export const ImgCard = ({ image, title, subTitle, link, ghLink }) => (
   <div
     className={
-      " relative place-content-center flex flex-col h-[600px] mb-4 hover:cursor-pointer hover:scale-[0.99] active:scale-[0.98] ease-in-out "
+      " group relative place-content-center flex flex-col h-[600px] mb-4 hover:cursor-pointer ease-in-out "
     }
   >
     <div
       className={
-        "w-auto h-full place-content-center bg-cover bg-center flex flex-col z-0 contrast-[30%] ease-in-out shadow-2xl hover:brightness-[80%] hover:shadow-xl rounded-lg border " +
+        "group w-auto h-full place-content-center bg-cover bg-center flex flex-col z-0 contrast-[30%] ease-in-out shadow-2xl hover:brightness-[80%] hover:shadow-xl rounded-lg border " +
         image
       }
       onClick={() => {window.open(link, "blank"); window.focus();}}
     ></div>
-    <div className=" opacity-100 z-10 blur-none absolute self-center text-white">
+    <div className=" opacity-100 z-10 blur-none absolute self-center text-white pointer-events-none">
       <div className="text-xl m-2 sm:text-3xl font-serif">{title}</div>
       <div className="text-m m-2 ">{subTitle}</div>
     </div>
